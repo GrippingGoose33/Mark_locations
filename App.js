f038b0f229be463440ec19d799ecd5867f5ed764
GrippingGoose33 <grippinggoose33@hotmail.com>
2022-07-11 12:44:29 -0500

    First commit

--- App.js
+++ App.js
@@ -1,20 +1,63 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import { Text, View } from 'react-native';
+import * as Location from 'expo-location';
+import styles from './styles';
+
+const API_KEY = "";
+const URL = `https://maps.google.com/maps/api/geocode/json?key=${API_KEY}&latlng`;
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  const [address, setAddress] = useState("loading...");
+  const [longitude, setLongitude] = useState();
+  const [latitude, setLatitude] = useState();
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  useEffect (() => {
+    function setPosition({coords: {latitude, longitude}}) {
+      setLongitude(longitude);
+      setLatitude(latitude);
+      fetch(`${URL}${latitude}${longitude}`)
+        .then((resp) => resp.json())
+        .then(({results}) => {
+          if(result.length > 0) {
+            setAddress(results[0].formatted_address);
+          }
+        })
+        .catch((error) => {
+          console.log(error.message);
+        });
+    }
+
+    let watcher;
+
+    (async () => {
+      let {status} = await Location.requestBackgroundPermissionsAsync();
+
+      if(status != "granted") {
+        serErrorMsg("Permision denied");
+        return; 
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setPosition(location);
+
+
+      watcher = await Location.watchPositionAsync(
+        {accuracy: Location.LocationAccuracy.Highest},
+      );
+    })();
+
+    return () => {
+      watcher?.remove();
+    };
+
+  }, []);
+
+  return (
+    <View style={styles.constainer}>
+      <Text style={styles.label}>Address: {address}</Text>
+      <Text style={styles.label}>Latitude: {Latitude}</Text>
+      <Text style={styles.label}>Longitude: {longitude}</Text>
+    </View>
+  )
+
+}
