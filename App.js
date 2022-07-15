6bab314b467cc0e27ec5bf11689ac9d99edbbaa3
GrippingGoose33 <grippinggoose33@hotmail.com>
2022-07-15 10:55:03 -0500

    Hotel-Bar

--- App.js
+++ App.js
@@ -6,11 +6,11 @@ import styles from './styles';
 
 const hotelRegion = {
   coordinates: [
-    {latitude: 1, longitude: 1},
-    {latitude: 1, longitude: 1},
-    {latitude: 1, longitude: 1},
-    {latitude: 1, longitude: 1},
-    {latitude: 1, longitude: 1},
+    {latitude: 20.672747278747842, longitude: -103.37539720734652},
+    {latitude: 20.669673831926215, longitude: -103.37494960785168},
+    {latitude: 20.67112893574988, longitude: -103.36900943150502},
+    {latitude: 20.673213307650773, longitude: -103.37109064046835},
+    {latitude: 20.67335383954868, longitude: -103.3691379924641},
   ],
 
   strokeColor: "#008080",
@@ -19,11 +19,11 @@ const hotelRegion = {
 
 const barRegion = {
   coordinates: [
-    {latitude: 1, longitude: 1},
-    {latitude: 1, longitude: 1},
-    {latitude: 1, longitude: 1},
-    {latitude: 1, longitude: 1},
-    {latitude: 1, longitude: 1},
+    {latitude: 20.671145466079626, longitude: -103.37277506730538},
+    {latitude: 20.667069928972786, longitude: -103.37072585978443},
+    {latitude: 20.66866100757824, longitude: -103.36802219329526},
+    {latitude: 20.671120370739587, longitude: -103.36820458349344},
+    {latitude: 20.671145466079626, longitude: -103.37277506730538},
   ],
 
   strokeColor: "firebrick",
@@ -32,8 +32,8 @@ const barRegion = {
 
 
 export default function App() {
-  const [hotelRegion, setHotelStyles] = useState([styles.hotelText, styles.boldText]);
-  const [barRegion, setBarStyles] = useState([styles.barText]);
+  const [hotelStyles, setHotelStyles] = useState([styles.hotelText, styles.boldText]);
+  const [barStyles, setBarStyles] = useState([styles.barText]);
   const [overlays, setOverlays] = useState([hotelRegion]);
 
   function onClickhotel() {
